7554bbd1d1d3ff0295c3528e6ab2ed52636a1a16
turbo-smurf <f.vendrig@gmail.com>
2026-09-04 13:52:15 +0200

    Update streamlit_app.py
    Fix API link

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,6 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
